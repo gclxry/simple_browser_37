48798f924d60d5b1a49d2900b3901b9ab9a4c369
plxaye <307685235@qq.com>
2014-09-30 17:59:18 +0800

    填充SimpleBrowserMainParts最基本内容，还不能编译通过

---
+++ browser/simple_browser_main_parts.cc
@@ -0,0 +1,102 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/simple/browser/simple_browser_main_parts.h"
+
+#include "base/base_switches.h"
+#include "base/bind.h"
+#include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/message_loop/message_loop.h"
+#include "base/threading/thread.h"
+#include "base/threading/thread_restrictions.h"
+#include "content/public/browser/browser_thread.h"
+#include "content/public/browser/storage_partition.h"
+#include "content/public/common/content_switches.h"
+#include "content/public/common/main_function_params.h"
+#include "content/public/common/url_constants.h"
+//#include "content/simple/browser/shell.h"
+#include "content/simple/browser/simple_browser_context.h"
+#include "content/simple/browser/simple_net_log.h"
+//#include "content/simple/common/simple_switches.h"
+#include "grit/net_resources.h"
+#include "net/base/filename_util.h"
+#include "net/base/net_module.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "url/gurl.h"
+#include "webkit/browser/quota/quota_manager.h"
+
+namespace content {
+
+  namespace {
+
+    // Default quota for each origin is 5MB.
+    const int kDefaultLayoutTestQuotaBytes = 5 * 1024 * 1024;
+
+    GURL GetStartupURL() {
+      CommandLine* command_line = CommandLine::ForCurrentProcess();
+      const CommandLine::StringVector& args = command_line->GetArgs();
+
+      if (args.empty())
+        return GURL("http://www.baidu.com/");
+
+      GURL url(args[0]);
+      if (url.is_valid() && url.has_scheme())
+        return url;
+
+      return net::FilePathToFileURL(base::FilePath(args[0]));
+    }
+
+    base::StringPiece PlatformResourceProvider(int key) {
+      if (key == IDR_DIR_HEADER_HTML) {
+        base::StringPiece html_data =
+          ui::ResourceBundle::GetSharedInstance().GetRawDataResource(
+          IDR_DIR_HEADER_HTML);
+        return html_data;
+      }
+      return base::StringPiece();
+    }
+
+  }  // namespace
+
+  SimpleBrowserMainParts::SimpleBrowserMainParts(
+    const MainFunctionParams& parameters)
+    : BrowserMainParts(), parameters_(parameters), run_message_loop_(true) {}
+
+  SimpleBrowserMainParts::~SimpleBrowserMainParts() {
+  }
+
+
+  void SimpleBrowserMainParts::PreMainMessageLoopRun() {
+    //net_log_.reset(new ShellNetLog("content_simple"));
+    browser_context_.reset(new SimpleBrowserContext(false, net_log_.get()));
+    off_the_record_browser_context_.reset(
+      new SimpleBrowserContext(true, net_log_.get()));
+
+    //Shell::Initialize();
+    //net::NetModule::SetResourceProvider(PlatformResourceProvider);
+
+    //Shell::CreateNewWindow(browser_context_.get(),
+    //  GetStartupURL(),
+    //  NULL,
+    //  MSG_ROUTING_NONE,
+    //  gfx::Size());
+
+    if (parameters_.ui_task) {
+      parameters_.ui_task->Run();
+      delete parameters_.ui_task;
+      run_message_loop_ = false;
+    }
+  }
+
+  bool SimpleBrowserMainParts::MainMessageLoopRun(int* result_code)  {
+    return !run_message_loop_;
+  }
+
+  void SimpleBrowserMainParts::PostMainMessageLoopRun() {
+    browser_context_.reset();
+    off_the_record_browser_context_.reset();
+  }
+
+}  // namespace
---
+++ browser/simple_browser_main_parts.h
@@ -0,0 +1,62 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_SIMPLE_BROWSER_SIMPLE_BROWSER_MAIN_PARTS_H_
+#define CONTENT_SIMPLE_BROWSER_SIMPLE_BROWSER_MAIN_PARTS_H_
+
+#include "base/basictypes.h"
+#include "base/memory/scoped_ptr.h"
+#include "content/public/browser/browser_main_parts.h"
+#include "content/public/common/main_function_params.h"
+
+
+namespace base {
+  class Thread;
+}
+
+namespace net {
+  class NetLog;
+}
+
+namespace content {
+
+  class SimpleBrowserContext;
+  //class SimpleDevToolsDelegate;
+  //class SimplePluginServiceFilter;
+
+  class SimpleBrowserMainParts : public BrowserMainParts {
+  public:
+    explicit SimpleBrowserMainParts(const MainFunctionParams& parameters);
+    virtual ~SimpleBrowserMainParts();
+
+    // BrowserMainParts overrides.
+    virtual void PreEarlyInitialization() OVERRIDE;
+    virtual void PreMainMessageLoopStart() OVERRIDE;
+    virtual void PostMainMessageLoopStart() OVERRIDE;
+    virtual void PreMainMessageLoopRun() OVERRIDE;
+    virtual bool MainMessageLoopRun(int* result_code) OVERRIDE;
+    virtual void PostMainMessageLoopRun() OVERRIDE;
+
+
+    SimpleBrowserContext* browser_context() { return browser_context_.get(); }
+    SimpleBrowserContext* off_the_record_browser_context() {
+      return off_the_record_browser_context_.get();
+    }
+
+    net::NetLog* net_log() { return net_log_.get(); }
+
+  private:
+    scoped_ptr<net::NetLog> net_log_;
+    scoped_ptr<SimpleBrowserContext> browser_context_;
+    scoped_ptr<SimpleBrowserContext> off_the_record_browser_context_;
+
+    // For running content_browsertests.
+    const MainFunctionParams parameters_;
+    bool run_message_loop_;
+    DISALLOW_COPY_AND_ASSIGN(SimpleBrowserMainParts);
+  };
+
+}  // namespace content
+
+#endif  // CONTENT_SIMPLE_BROWSER_SIMPLE_BROWSER_MAIN_PARTS_H_
